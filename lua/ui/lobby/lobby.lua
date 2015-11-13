0b945b4bc10e17d4cd06084808a5e61d493ea095
Sheeo <sheeo@faforever.com>
2015-11-14 00:09:21 +0100

    Remove "Connected" messages

--- lua/ui/lobby/lobby.lua
+++ lua/ui/lobby/lobby.lua
@@ -3153,7 +3153,6 @@ function CalcConnectionStatus(peer)
             end
 
             table.insert(connectedTo, peer.id)
-            GpgNetSend('Connected', string.format("%d", peer.id))
         end
         if not table.find(peer.establishedPeers, lobbyComm:GetLocalPlayerID()) then
             -- they haven't reported that they can talk to us?
@@ -3425,7 +3424,6 @@ function InitLobbyComm(protocol, localPort, desiredPlayerName, localPlayerUID, n
         localPlayerID = myID
         localPlayerName = myName
 
-        GpgNetSend('connectedToHost', string.format("%d", hostID))
         lobbyComm:SendData(hostID, { Type = 'SetAvailableMods', Mods = Mods.GetLocallyAvailableMods(), Name = localPlayerName} )
 
         lobbyComm:SendData(hostID,
